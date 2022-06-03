d6419e7e4edd4d4a41500a4d79f7f932c257888c
alexandra-andreea.albu <albua@haufe.com>
2022-06-03 15:59:45 +0300

    add webpage & server skeletons

---
+++ hack-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Map from './components/Map';
+import DeleteMe from './components/DeleteMe';
+import './App.css';
+
+// TODO: start here
+
+function App() {
+  return (
+    <>
+      <Map/>
+      <DeleteMe/>
+    </>
+  );
+}
+
+export default App;
---
+++ hack-app/src/components/Map.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+// Don't forget to import Map.css, or else your map will not appear!
+import './Map.css';
+
+const Haufe = [45.75951, 21.220871];
+
+function Map() {
+  return (
+    <MapContainer 
+      center={Haufe} 
+      zoom={13} 
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+      />
+      <Marker
+          position={Haufe}
+          icon={new Icon({
+            iconUrl: "/pin.svg",
+            iconSize: [25, 25]
+          })}
+        >
+        <Popup>
+          Haufe Hackaton
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+export default Map;
